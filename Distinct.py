ca27ba184ac6e486a03f769fba0c5c373191ed34
MrReese0 <42744463+MrReese0@users.noreply.github.com>
2018-08-27 22:11:42 -0700

    Create Distinct.py

---
+++ Distinct.py
@@ -0,0 +1,9 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+def solution(A):
+    # write your code in Python 3.6
+    distinct = {}
+    for val in A:
+        distinct[val] = True
+    return len(distinct.keys())
