53401fadda1beaa21b5dbc9479572e27c909d901
MrReese0 <42744463+MrReese0@users.noreply.github.com>
2018-09-12 21:34:12 -0700

    Create CountFactors.py

---
+++ CountFactors.py
@@ -0,0 +1,11 @@
+def solution(N):
+    count = 0
+    i = 1
+    while ( i * i <= N):
+        if (N % i == 0):
+            if i * i == N:
+               count += 1
+            else:
+                count += 2
+        i += 1
+    return count
